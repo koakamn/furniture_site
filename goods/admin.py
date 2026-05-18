e64acd795b241c23beb83a5e21409b91716d8765
koakamn <talgatsamir@gmail.com>
2026-05-18 12:26:11 +0500

    adding fixtures and register models in admin site

--- goods/admin.py
+++ goods/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from goods.models import *
 
-admin.site.register(Categories)
-admin.site.register(Product)
+# admin.site.register(Categories)
+# admin.site.register(Product)
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
