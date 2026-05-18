27b73ad91b0c508730e8ac258b8a61abb81ce920
koakamn <talgatsamir@gmail.com>
2026-05-18 12:35:23 +0500

    debug toolbar

--- app/settings.py
+++ app/settings.py
@@ -26,6 +26,9 @@ SECRET_KEY = 'django-insecure-$3tkj430fbmaq-60f$p%2g#^^(%)=s-dtart_(*giq*2_a_r-2
 DEBUG = True
 
 ALLOWED_HOSTS = []
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -37,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'debug_toolbar',
+
     'main',
     'goods',
 
@@ -50,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
--- app/urls.py
+++ app/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from app.settings import DEBUG
 
 from main import views
 
@@ -23,5 +24,10 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('main.urls',namespace='main')),
-    path('catalog/',include('goods.urls',namespace='catalog')),
+    path('catalog/',include('goods.urls',namespace='catalog'))
 ]
+
+if DEBUG:
+    urlpatterns+=[
+        path('__debug__/',include('debug_toolbar.urls',namespace='debug_toolbar')),
+    ]
