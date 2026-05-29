e79499709dac43a09cbfbf6b6dbba1c22d317111
koakamn <talgatsamir@gmail.com>
2026-05-29 19:59:15 +0500

    configure media files

--- app/settings.py
+++ app/settings.py
@@ -126,3 +126,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static',]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from app.settings import DEBUG
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 from main import views
 
@@ -27,7 +29,8 @@ urlpatterns = [
     path('catalog/',include('goods.urls',namespace='catalog'))
 ]
 
-if DEBUG:
-    urlpatterns+=[
+if settings.DEBUG:
+    urlpatterns+=([
         path('__debug__/',include('debug_toolbar.urls',namespace='debug_toolbar')),
-    ]
+    ])
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
