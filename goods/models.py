7ffe58a37d3ef7842d6ac245abbeb49a4c86a818
koakamn <talgatsamir@gmail.com>
2026-05-29 23:18:21 +0500

    discount logic

--- goods/models.py
+++ goods/models.py
@@ -25,9 +25,18 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
     class Meta:
         db_table = 'product'
         verbose_name = 'Продукт'
         verbose_name_plural='Продукты'
+
+    def display_id(self):
+        return f"{self.id:05}"
+
+    def sell_price(self):
+        if self.discount:
+            return round(self.price - self.price * self.discount/100,2)
+        return self.price
 
 
