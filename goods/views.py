a3ecfffecdc5957222de54a4b1be5d26f6a39d03
koakamn <talgatsamir@gmail.com>
2026-07-08 20:51:39 +0500

    search for products

--- goods/views.py
+++ goods/views.py
@@ -2,16 +2,21 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
 from goods.models import Product
+from goods.utils import q_search
 
-def catalog(request,category_slug):
+
+def catalog(request,category_slug=None):
 
     page=request.GET.get('page',default=1)
     on_sale = request.GET.get('on_sale', default=None)
     order_by = request.GET.get('order_by', default=None)
+    query=request.GET.get('q',default=None)
 
 
     if category_slug=='all':
         goods=Product.objects.all()
+    elif query:
+        goods=q_search(query)
     else:
         goods=get_list_or_404(Product.objects.filter(category__slug=category_slug))
 
