5c4b824a0e969131cc2ddd2a282b71968084849c
koakamn <talgatsamir@gmail.com>
2026-07-08 17:49:56 +0500

    product filtering and urlencode for pagination

--- goods/views.py
+++ goods/views.py
@@ -1,14 +1,24 @@
+
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
 from goods.models import Product
 
 def catalog(request,category_slug):
+
     page=request.GET.get('page',default=1)
+    on_sale = request.GET.get('on_sale', default=None)
+    order_by = request.GET.get('order_by', default=None)
+
 
     if category_slug=='all':
         goods=Product.objects.all()
     else:
         goods=get_list_or_404(Product.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        goods=goods.filter(discount__gt=0)
+    if order_by and order_by!='default':
+        goods=goods.order_by(order_by)
 
     paginator=Paginator(goods,3)
     current_page=paginator.page(page)
